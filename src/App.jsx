7cb1401ad35134e7bb5abc4e0c763c1d67b4ea2d
zolppy <gabriel.lcifba@gmail.com>
2024-02-28 12:51:47 -0300

    esboçado grid-layout para desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+import "./_App.scss";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias maxime at, rem consequatur eligendi incidunt mollitia earum placeat, voluptas cupiditate, provident impedit quae adipisci perspiciatis! Tenetur facilis enim rem delectus.</p>
-    </React.Fragment>
+    <div className="App">App</div>
   );
 }
 
